749cb78bc5545324cbc8a28025b600ff7b5a8c36
user_106 <idan.fr@campus.technion.ac.il>
2022-03-25 19:58:10 +0300

    started to build state and agent

--- utils/stock_state.py
+++ utils/stock_state.py
@@ -1,6 +1,10 @@
+import torch
+from torch_geometric.data import Data
+
 class stock_state():
-    def __init__(self, stock_name, num_of_stocks=0, stock_value=0, total_value=0, lots=[]):
+    def __init__(self, stock_name, stock_type="stock", num_of_stocks=0, stock_value=0, total_value=0, lots=[]):
         self.stock_name    = stock_name
+        self.stock_type    = stock_type
         self.num_of_stocks = num_of_stocks
         self.stock_value   = stock_value
         self.total_value   = total_value
@@ -8,6 +12,8 @@ class stock_state():
 
     def buy_stocks(self, amount, value, date):
         self.num_of_stocks += amount
+        if (self.stock_type == "money"):
+            return
         self.total_value   = self.num_of_stocks * value
         self.stock_value   = value
 
@@ -15,7 +21,10 @@ class stock_state():
         self.lots.append(lot)
 
     def sell_stocks(self, amount, value):
-        assert amount <= self.num_of_stocks, "Selling Amount"
+        assert amount <= self.num_of_stocks, "error of amount in sell"
+        if self.stock_type=="money":
+            self.num_of_stocks -= amount
+            return amount
         self.stock_value = value
         selling_value  = 0
         selling_amount = 0
@@ -28,6 +37,11 @@ class stock_state():
                 selling_amount = amount
                 self.lots[0]['amount'] -= selling_amount
 
-            selling_value += selling_amount*lot['value']
+            selling_value += selling_amount*value
             amount -= selling_amount
+        return selling_value
 
+# class state():
+#     edge_index = torch.tensor([[0, 1, 1, 2, 0, 2],
+#                                [1, 0, 2, 1, 2, 0]], dtype=torch.long)
+#     nodes = torch.tensor([[stock_state("UPRO", num_of_stocks=0, stock_value=0, total_value=0, lots=[]), ]])
